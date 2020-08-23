8f2ea2e0258e30174428910adff88d78b149e697
Swhite9478 <stevewhite.dev@gmail.com>
2020-08-23 13:27:23 -0700

    inital commit

---
+++ src/main/java/com/swhite/datingtracker/DatingTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.swhite.datingtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatingTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swhite/datingtracker/DatingTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swhite.datingtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
