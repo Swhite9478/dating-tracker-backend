0594ecaffea12b32fc438e0d2d72d65df87f0e47
Swhite9478 <stevewhite.dev@gmail.com>
2020-08-23 14:04:51 -0700

    added database information

--- src/test/java/com/swhite/datingtracker/DatingTrackerApplicationTests.java
+++ src/test/java/com/swhite/datingtracker/DatingTrackerApplicationTests.java
@@ -1,9 +1,16 @@
 package com.swhite.datingtracker;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DatingTrackerApplication.class)
+@ActiveProfiles("local")
 class DatingTrackerApplicationTests {
 
     @Test
